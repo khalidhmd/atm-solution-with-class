2a796738aefe6e65e1a3e9e151f5e88d69cd3697
khalidhmd <khalidhmd@outlook.com>
2018-02-12 17:40:39 +0200

    second test for .gitignore

--- atm_operation.py
+++ atm_operation.py
@@ -4,7 +4,8 @@ smart_bank_atm= atm_calss.ATM(7000, 'Smart Bank')
 
 baraka_bank_atm.withdraw(155)
 baraka_bank_atm.withdraw(456)
-
+baraka_bank_atm.withdraw(987)
+baraka_bank_atm.withdraw(234)
 #smart_bank_atm.withdraw(333)
 baraka_bank_atm.show_withdrawals()
 
